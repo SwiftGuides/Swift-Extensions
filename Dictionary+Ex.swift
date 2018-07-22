159e66403e0de087d88b939354e21d9d50550d79
iAugus <iAugux@gmail.com>
2018-07-22 20:45:21 +0800

    -

---
+++ Dictionary+Ex.swift
@@ -0,0 +1,20 @@
+//
+//  Dictionary+Ex.swift
+//
+//  Created by Augus on 7/22/18.
+//  Copyright © 2018 iAugus. All rights reserved.
+//
+
+import Foundation
+
+func += <K, V> (lhs: inout [K: V], rhs: [K: V]) {
+    for (k, v) in rhs {
+        lhs[k] = v
+    }
+}
+
+func + (_ lhs: [String: Any], _ rhs: [String: Any]) -> [String: Any] {
+    var temp = lhs
+    temp += rhs
+    return temp
+}
